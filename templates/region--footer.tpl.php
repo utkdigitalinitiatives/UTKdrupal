3c02d67625cac854e469d8e8e5214c2a176e45e0
Dan Greene <dgreene@utk.edu>
2016-10-18 14:47:34 -0400

    clean up university footer

--- templates/region--footer.tpl.php
+++ templates/region--footer.tpl.php
@@ -5,31 +5,33 @@
 <!--  Begin Libraries footer -->
 
 <!--  begin #colophon -->
-    <footer id="colophon" class="site-footer" role="contentinfo">
+    <footer id="site-footer" role="contentinfo">
   <div id="siteinfo">
-		<div id="meta-info">
-      <p>
-        <strong class="sitetile"><a href="https://www.lib.utk.edu">University Libraries</a></strong><br>
-</p>
+		<div class="footer-column">
+      <h2><a href="https://www.lib.utk.edu">University Libraries</a></h2>
 		</div>
-    <div id="meta-contact">
-       <p>1015 Volunteer Boulevard <br />
+    <div class="footer-column">
+       <p>
+1015 Volunteer Boulevard <br />
 Knoxville, TN 37996-1000<br /> 
 Phone: (865) 974-4351<br />
-<a href="https://www.lib.utk.edu/contact">Contact Us</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.lib.utk.edu/contact/website-feedback">Website Feedback</a><br />
-<a href="https://www.facebook.com/utklibraries"><img src="https://www.lib.utk.edu/template/2016/images-utlibraries/social-media/facebook-16.png" title="UT Libraries on Facebook" alt="UT Libraries on Facebook" width="16" height="16" /></a>
-          &nbsp;
-          <a href="https://foursquare.com/v/john-c-hodges-library/4b16792cf964a52077b923e3"><img src="https://www.lib.utk.edu/template/2016/images-utlibraries/social-media/foursquare-16.png" title="UT Libraries on Foursquare" alt="UT Libraries on Foursquare" width="16" height="16" /></a>
-                &nbsp;
-                <a href="https://instagram.com/utklibraries/"><img src="https://www.lib.utk.edu/template/2016/images-utlibraries/social-media/instagram-16.png" title="UT Libraries on Instagram" alt="UT Libraries on Instagram" width="16" height="16" /></a>
-                        &nbsp;
-                          <a href="https://pinterest.com/utklibraries"><img src="https://www.lib.utk.edu/template/2016/images-utlibraries/social-media/pinterest-16.png" title="UT Libraries on Pinterest" alt="UT Libraries on Pinterest" width="16" height="16" /></a>
-        &nbsp;
-        <a href="https://twitter.com/utklibraries"><img src="https://www.lib.utk.edu/template/2016/images-utlibraries/social-media/twitter-16.png" title="UT Libraries on Twitter" alt="UT Libraries on Twitter" width="16" height="16" /></a>
-        &nbsp;
-        <a href="https://www.youtube.com/user/utklibraries"><img src="https://www.lib.utk.edu/template/2016/images-utlibraries/social-media/youtube-16.png" title="UT Libraries on YouTube" alt="UT Libraries on YouTube" width="16" height="16" /></a>
-		        &nbsp;
-		        <a href="https://itunes.apple.com/us/app/tennessee/id548076679?mt=8"><img src="https://www.lib.utk.edu/template/2016/images-utlibraries/social-media/ut-16.png" title="University of Tennessee iOS App" alt="University of Tennessee iOS App" width="16" height="16" /></a></p>
+<a href="https://www.lib.utk.edu/contact">Contact Us</a> <span id="pipe-space">|</span><a href="https://www.lib.utk.edu/contact/website-feedback">Website Feedback</a><br />
+
+<a href="https://www.facebook.com/utklibraries"><img src="https://www.lib.utk.edu/template/2016/images-utlibraries/social-media/facebook-16.png" class="social-media-icon" title="UT Libraries on Facebook" alt="UT Libraries on Facebook" /></a>
+          
+          <a href="https://foursquare.com/v/john-c-hodges-library/4b16792cf964a52077b923e3"><img src="https://www.lib.utk.edu/template/2016/images-utlibraries/social-media/foursquare-16.png" class="social-media-icon" title="UT Libraries on Foursquare" alt="UT Libraries on Foursquare" /></a>
+                
+                <a href="https://instagram.com/utklibraries/"><img src="https://www.lib.utk.edu/template/2016/images-utlibraries/social-media/instagram-16.png" class="social-media-icon" title="UT Libraries on Instagram" alt="UT Libraries on Instagram" /></a>
+                        
+                          <a href="https://pinterest.com/utklibraries"><img src="https://www.lib.utk.edu/template/2016/images-utlibraries/social-media/pinterest-16.png" class="social-media-icon" title="UT Libraries on Pinterest" alt="UT Libraries on Pinterest" /></a>
+       
+        <a href="https://twitter.com/utklibraries"><img src="https://www.lib.utk.edu/template/2016/images-utlibraries/social-media/twitter-16.png" class="social-media-icon"  title="UT Libraries on Twitter" alt="UT Libraries on Twitter" /></a>
+       
+        <a href="https://www.youtube.com/user/utklibraries"><img src="https://www.lib.utk.edu/template/2016/images-utlibraries/social-media/youtube-16.png" class="social-media-icon" title="UT Libraries on YouTube" alt="UT Libraries on YouTube" /></a>
+		      
+		        <a href="https://itunes.apple.com/us/app/tennessee/id548076679?mt=8"><img src="https://www.lib.utk.edu/template/2016/images-utlibraries/social-media/ut-16.png" class="social-media-icon" title="University of Tennessee iOS App" alt="University of Tennessee iOS App" width="16" height="16" /></a>
+		        
+		        </p>
     </div>
 
    </div>
@@ -37,16 +39,17 @@ Phone: (865) 974-4351<br />
 
 <!-- Here Begins The Campus Footer. Do not touch below this line -->
 <div id="campus-footer">
-   <div id="utk">
-     <div id="bobi">
-        <h2><a href="http://www.utk.edu" class="logo icon-bobi-main">The University of Tennessee</a></h2>
+   <div class="footer-column">
+     <div id="big-orange-logo">
+        <h2><a href="http://www.utk.edu">The University of Tennessee</a></h2>
       </div>
       <div id="address">
-          <p><strong>The University of Tennessee, Knoxville</strong><br>Knoxville, Tennessee 37996<br> 865-974-1000</p>
+          <p><strong>The University of Tennessee, Knoxville</strong><br>Knoxville, Tennessee 37996<br /> 865-974-1000</p>
       </div>
     </div>
 
-    <div id="toolkit">
+   <div class="footer-column">
+
       <form method="post" action="http://google.tennessee.edu/search">
           <div class="form-group">
              <input type="text" class="form-control" name="q"  maxlength="256" onfocus="if(this.value == 'Search utk.edu') { this.value = ''; }" value="Search utk.edu" title="Search UT Knoxville">
@@ -60,12 +63,11 @@ Phone: (865) 974-4351<br />
           <input type="hidden" name="site" value="Knoxville">
           <input type="hidden" name="client" value="utk_translate_frontend">
           <input type="hidden" name="entqr" value="3">
-          <!--    <input type="hidden" name="sitesearch" value="utk.edu" /> -->
           <input type="hidden" name="qtype" class="searchtext" value="utk" title="search type">
           <input type="hidden" name="proxystylesheet" value="utk_translate_frontend">
       </form>
       <br>
-      <nav  role="navigation">
+      <div id ="university-links">
           <ul>
             <li><a href="http://www.utk.edu/events/">Events</a></li>
             <li><a href="http://www.utk.edu/maps/">Map</a></li>
@@ -81,13 +83,15 @@ Phone: (865) 974-4351<br />
             <a href="http://giveto.utk.edu">Give to UT</a>
             </li>
                       </ul>  
-      </nav>
+      </div>
     </div>
-</div>  
-</footer><!--  end #colophon -->
-<!--  end footer include -->
+</div>
 
 <div id="system-indicia">
 	<p>The flagship campus of <a href="http://tennessee.edu">the University of Tennessee System</a> and partner in <a href="http://www.tntransferpathway.org/">the Tennessee Transfer Pathway</a>.</p>
-  </div>
+  </div>  
+</footer><!--  end #colophon -->
+<!--  end footer include -->
+
+
 <!-- The UT Footer ends here. -->
