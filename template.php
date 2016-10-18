14eaf41274c4c2b213d2031a1045efb8228db7f3
Don Richards <highlander411@hotmail.com>
2016-10-18 08:30:04 -0400

    Create template.php

---
+++ template.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ * Template.php - functions to manipulate Drupal's default markup.
+ *
+*/
+
+/**
+ * Modify any theme hooks variables or add your own variables, using preprocess or process functions.
+ * Override any theme function. That is, replace a module's default theme function with one you write.
+ * Call hook_*_alter() functions which allow you to alter various parts of Drupal's internals, including 
+ * the render elements in forms. The most useful of which include hook_form_alter(), 
+ * hook_form_FORM_ID_alter(), and hook_page_alter(). See api.drupal.org for more information about 
+ * _alter functions. Or link to this file's description https://www.drupal.org/node/1728096
+*/
