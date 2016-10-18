910a408d56e3feb94d9f6669fad07f30e15ba937
Dan Greene <dgreene@utk.edu>
2016-10-18 09:47:56 -0400

    Set to Drupal defaults. Reference: https://api.drupal.org/api/drupal/modules!system!html.tpl.php/7.x

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -49,13 +49,17 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
 
 <head profile="<?php print $grddl_profile; ?>">
+	
+<?php print $head; ?>
 
-  <title><?php print $head_title; ?></title>
+<title><?php print $head_title; ?></title>
 
+<?php print $styles; ?>
   
-  <?php print $styles; ?>
-  <?php print $scripts; ?>
+<?php print $scripts; ?>
+
 </head>
+
 <body class="<?php print $classes; ?>" <?php print $attributes; ?>>
  
   <?php print $page_top; ?>
@@ -63,5 +67,6 @@
   <?php print $page; ?>
  
   <?php print $page_bottom; ?>
+
 </body>
 </html>
