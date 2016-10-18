4ef9d187ebd19c9e5fd71c7d5078a1ac7f79e69b
Dan Greene <dgreene@utk.edu>
2016-10-18 13:10:31 -0400

    fix php tag in footer

--- templates/region--footer.tpl.php
+++ templates/region--footer.tpl.php
@@ -1,4 +1,4 @@
-<?php
+<?php ?>
 
 <!-- The UT Footer starts here. -->
 
@@ -91,5 +91,3 @@ Phone: (865) 974-4351<br />
 	<p>The flagship campus of <a href="http://tennessee.edu">the University of Tennessee System</a> and partner in <a href="http://www.tntransferpathway.org/">the Tennessee Transfer Pathway</a>.</p>
   </div>
 <!-- The UT Footer ends here. -->
-
-?>
