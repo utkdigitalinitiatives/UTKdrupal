8482f8fd73c5af085c24a7c1c4bb7c5c15c289da
Don Richards <highlander411@hotmail.com>
2016-10-28 12:12:37 -0400

    Add Search Button Changes

--- template.php
+++ template.php
@@ -17,3 +17,25 @@ function UTKdrupal_preprocess_page(&$variables) {
   global $base_path;
   $variables['logopath'] = $base_path.'/' . drupal_get_path('theme','UTKdrupal') . '/logo.png';
 }
+
+
+/**
+* hook_form_FORM_ID_alter
+*/
+function UTKdrupal_form_search_block_form_alter(&$form, &$form_state, $form_id) {
+    $form['search_block_form']['#title'] = t('Search'); // Change the text on the label element
+    $form['search_block_form']['#title_display'] = 'invisible'; // Toggle label visibilty
+    $form['search_block_form']['#size'] = 20;  // define size of the textfield
+    $form['search_block_form']['#default_value'] = t('Search'); // Set a default value for the textfield
+    // $form['actions']['submit']['#value'] = t('GO!'); // Change the text on the submit button
+    // $form['actions']['submit'] = array('#type' => 'image_button', '#src' => base_path() . path_to_theme() . '/images/search-button.png');
+
+    // Add extra attributes to the text box
+    $form['search_block_form']['#attributes']['onblur'] = "if (this.value == '') {this.value = 'Search';}";
+    $form['search_block_form']['#attributes']['onfocus'] = "if (this.value == 'Search') {this.value = '';}";
+    // Prevent user from searching the default text
+    $form['#attributes']['onsubmit'] = "if(this.search_block_form.value=='Search'){ alert('Please enter a search'); return false; }";
+
+    // Alternative (HTML5) placeholder attribute instead of using the javascript
+    $form['search_block_form']['#attributes']['placeholder'] = t('Search');
+}
