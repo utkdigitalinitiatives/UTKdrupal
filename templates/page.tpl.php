f21911d942a6c64596bca2e576b122cabb846292
cdeaneGit <cdeane@utk.edu>
2016-11-02 16:13:58 -0400

    Trac-212 Collection Page Submit button - revise code in page.tpl.php to correct this link target.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -186,8 +186,7 @@
         if ($page['sidebar_second']): ?>
             <div class="left-sidebar">	
 	    <ul class="action-links">
-	    <li><a href="/islandora/object/islandora%3Agradthes/manage/overview/ingest">Submit</a></li> 
-	<li><a href="/islandora/object/utk.ir%3Atd#overlay=islandora/object/utk.ir%253Atd/manage/overview/ingest">Submit2</a></li>
+	<li><a href="/islandora/object/utk.ir%3Atd#overlay=islandora/object/utk.ir%253Atd/manage/overview/ingest">Submit</a></li>
 						
 	</ul>
                 <?php
