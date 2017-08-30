cf52c3cfac62b9960f0424747cc287b20bed22be
Don Richards <highlander411@hotmail.com>
2017-08-30 09:30:28 -0400

    Revert "Add Google Analytics TRAC-834"
    This reverts commit 23575c67931d992915a03e8f2f4fc46f4a53d35b.

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -47,15 +47,6 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
 
 <head profile="<?php print $grddl_profile; ?>">
-
-    <!-- Google Tag Manager -->
-  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-WQMZW84');</script>
-  <!-- End Google Tag Manager -->
-
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   	<link rel="profile" href="http://gmpg.org/xfn/11">
@@ -92,12 +83,6 @@
 </head>
 <body class="dropnav page-template-template-nosidebar webapp <?php print $classes; ?>" <?php print $attributes;?>>
  
-
-  <!-- Google Tag Manager (noscript) -->
-  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WQMZW84"
-  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-  <!-- End Google Tag Manager (noscript) -->
-
   <?php print $page_top; ?>
 
   <?php print $page; ?>
