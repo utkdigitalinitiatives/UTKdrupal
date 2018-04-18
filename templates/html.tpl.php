ac23b84fb858baf0799a55cdab580f86633855b2
pc37utn <pc37utn@users.noreply.github.com>
2018-04-18 13:24:39 -0400

    Add GTM code to html.tpl.php per TRAC-1102

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -46,6 +46,13 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
   <head profile="<?php print $grddl_profile; ?>">
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-WQMZW84');</script>
+    <!-- End Google Tag Manager -->
     <?php print $head; ?>
     <title><?php print $head_title; ?></title>
     <?php print $styles; ?>
@@ -53,6 +60,10 @@
   </head>
 
   <body class="dropnav page-template-template-nosidebar webapp <?php print $classes; ?>" <?php print $attributes;?>>
+   <!-- Google Tag Manager (noscript) -->
+   <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WQMZW84"
+   height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+   <!-- End Google Tag Manager (noscript) -->
     <?php include $directory . '/templates/includes/page_top.tpl.inc'; ?>
     <?php print $page_top; ?>
     <?php print $page; ?>
